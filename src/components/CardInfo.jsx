325737e3cbc81540627df4cd0d0b52fa15f9f945
jannickeham <jannicke.hamarsnes@gmail.com>
2022-04-22 17:35:31 +0200

    Changed favicon and title to document

---
+++ src/components/CardInfo.jsx
@@ -0,0 +1,17 @@
+import Church from "./Church";
+import Location from "./Location";
+import Hotel from "./Hotel"
+import Other from "./Other";
+
+
+
+function CardInfo(){
+    
+    return(
+    <>
+        <div>sldkfj</div>
+    </>
+    )
+}
+
+export default CardInfo;
